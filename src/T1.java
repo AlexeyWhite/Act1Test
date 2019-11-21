4b8f0bcf1e0400632c75c0dc7e1e49f600f18df5
ias <ias@sigma-soft.spb.ru>
2019-11-21 18:22:48 +0300

    .

---
+++ src/T1.java
@@ -0,0 +1,11 @@
+import net.sf.sojo.interchange.json.JsonParser;
+
+public class T1 {
+
+	public static void main(String[] args) {
+		Object obj;
+		// TODO Auto-generated method stub
+		System.out.print(obj = new JsonParser().parse("{\"test\":1}"));
+	}
+
+}
